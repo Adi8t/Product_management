d1111e8a33708345cabe9c60837dcfb6c3768eb9
Adi8t <adityrangi2@gmail.com>
2025-08-21 19:09:30 +0530

    database changes

--- core/settings.py
+++ core/settings.py
@@ -74,13 +74,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectdb',  # Name of your PostgreSQL database
+        'USER': 'postgres',  # PostgreSQL username
+        'PASSWORD': 'postgres', # PostgreSQL password
+        'HOST': 'localhost',  # Or the IP/hostname of your PostgreSQL server
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
