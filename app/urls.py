28dc53f4cecddb4162d8cc7f55555f9c90faafdf
Adi8t <adityrangi2@gmail.com>
2025-08-22 16:58:03 +0530

    create apis and serializer for Product model

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import CategoryAPIView
+from django.urls import path
+from .views import ProductAPIView
 urlpatterns = [
    path('api/categories/', CategoryAPIView.as_view()),
-   path("api/categories/<int:pk>/", CategoryAPIView.as_view())
+   path("api/categories/<int:pk>/", CategoryAPIView.as_view()),
+    # for product
+    path("api/products/", ProductAPIView.as_view()),
+    path("api/products/<int:pk>/", ProductAPIView.as_view()),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
-from app.serializer import CategorySerializer
+from app.serializer import CategorySerializer, ProductSerializer
 from rest_framework.views import APIView
-from app.models import Category
+from app.models import Category, Product
 
 
 class CategoryAPIView(APIView):
@@ -44,5 +44,30 @@ class CategoryAPIView(APIView):
 
 
 
+# for Crud api product 
 
+class ProductAPIView(APIView):
+    def get(self, request, pk):
+            product = Product.objects.get(pk=pk)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(created_by=request.user) 
+            return Response(serializer.data)
+        return Response(serializer.errors,)
 
+    def put(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save(created_by=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, )
+
+    def delete(self, request, pk):
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response({"message": "Product deleted"})
